f4b5f05a0cd6474be3b79d2b10fe6e341d50a25c
Matthew J Mucklo <mmucklo@corp.oodle.com>
2013-03-06 05:58:24 -0800

    add requiring of krumo to bundle constructor - should alleviate some issues

--- OodleKrumoBundle.php
+++ OodleKrumoBundle.php
@@ -7,4 +7,8 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 class OodleKrumoBundle
     extends Bundle
 {
+    public function __construct()
+    {
+        require_once(__DIR__ . '/../../../krumo/class.krumo.php');
+    }
 }
